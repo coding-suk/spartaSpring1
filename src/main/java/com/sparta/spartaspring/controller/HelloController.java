d5f976fc11fe81b4302a9c9bfd3f909b3b8dbc1c
coding-suk <hjldd0523@naver.com>
2024-08-14 10:34:37 +0900

    Modify Controller

--- src/main/java/com/sparta/spartaspring/controller/HelloController.java
+++ src/main/java/com/sparta/spartaspring/controller/HelloController.java
@@ -1,4 +1,4 @@
-package com.sparta.spartaspring.contriller;
+package com.sparta.spartaspring.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
